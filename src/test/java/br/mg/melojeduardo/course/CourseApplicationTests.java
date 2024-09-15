9e79712e293a3644d3f38dae515bd2878b590a74
Jose Melo <melo.jeduardo@gmail.com>
2024-09-15 09:38:40 -0300

    Project created

---
+++ src/test/java/br/mg/melojeduardo/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mg.melojeduardo.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
