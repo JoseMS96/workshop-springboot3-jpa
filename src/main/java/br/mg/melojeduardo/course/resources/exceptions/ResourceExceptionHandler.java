e6eabb6ae062520086259e2d7102544f27ee5cfd
Jose Melo <melo.jeduardo@gmail.com>
2024-09-19 09:33:58 -0300

    Exception handling - findById

---
+++ src/main/java/br/mg/melojeduardo/course/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package br.mg.melojeduardo.course.resources.exceptions;
+
+import br.mg.melojeduardo.course.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String errorName = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), errorName, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
--- src/main/java/br/mg/melojeduardo/course/services/UserService.java
+++ src/main/java/br/mg/melojeduardo/course/services/UserService.java
@@ -2,6 +2,7 @@ package br.mg.melojeduardo.course.services;
 
 import br.mg.melojeduardo.course.entities.User;
 import br.mg.melojeduardo.course.repositories.UserRepository;
+import br.mg.melojeduardo.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj ){
